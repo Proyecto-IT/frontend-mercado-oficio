dc16fa60f4523e4e2bf868933bf0f5b9440d0e2a
ElZorro <thiago2007crackz@gmail.com>
2025-10-13 23:28:58 -0300

    Update apiClient.ts

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -4,6 +4,7 @@ import { store } from '../store/store';
 import { updateAccessToken, logout } from '../store/authSlice';
 
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API URL:", API_URL);
 
 interface RefreshResponse {
   accessToken: string;
